36140bd8e788d7745eb666e13e546eb18ae5d9ef
Lucas Simidu <lucas.simidu@outlook.com>
2024-06-02 22:03:39 -0300

    Att alert

---
+++ assets/script/radius.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.querySelector('.container');
+   
+    container.style.borderTopLeftRadius = '20px';
+    container.style.borderTopRightRadius = '20px';
+    container.style.borderBottomLeftRadius = '0';
+    container.style.borderBottomRightRadius = '0';
+});
